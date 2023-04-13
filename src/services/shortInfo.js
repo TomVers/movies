a5e5c05885bd9143286473988ad4d78e68b2419b
Tom <tom@Toms-MacBook-2.local>
2023-04-13 19:41:41 +0400

    Mobile adaptive

--- src/services/shortInfo.js
+++ src/services/shortInfo.js
@@ -1,15 +1,11 @@
 export default function shortInfo(overview) {
   const maxLength = 170
   if (overview.length <= maxLength) {
-    return <p className='case__info'>{overview}</p>
+    return overview
   }
 
-  // Trim the text to the maximum length
   let shortenedText = overview.substr(0, maxLength)
-
-  // Re-trim if we are in the middle of a word
   shortenedText = shortenedText.substr(0, Math.min(shortenedText.length, shortenedText.lastIndexOf(' ')))
-
   shortenedText += ' ...'
 
   return shortenedText
